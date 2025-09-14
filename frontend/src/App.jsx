b6495c5e66e7a2b7ea9368077824b5bf325247d3
Nirmal-Kharal <nirmalkharal40@gmail.com>
2025-09-14 19:25:51 +0545

    feat(Navbar): add navbar and played with index.css

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,7 +10,7 @@ function App() {
   return (
     <AuthProvider>
       <Router>
-        <div className="min-h-screen bg-gray-50">
+        <div className="min-h-screen">
           <Navbar/>
           <div className="container mx-auto px-4 py-8">
             <Routes>
--- frontend/src/components/Layout/Navbar.jsx
+++ frontend/src/components/Layout/Navbar.jsx
@@ -1,8 +1,52 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { useAuth } from '../../context/AuthContext'
 
 const Navbar = () => {
+  const { isAuthenticated, user, logout } = useAuth();
+
+  const handleLogout = () => {
+    logout();
+  }
+
   return (
-    <div>Navbar</div>
+    <nav className=' text-white shadow-lg'>
+      <div className='container mx-auto px-4 '>
+        <div className='flex justify-between item-center h-16'>
+
+          {/* Logo Section */}
+          <Link to="/" className=' flex flex-center '  ><img className='h-12 ' src="/ZapLINK-T.png" alt="" /></Link>
+
+
+          {/* Navigation Links */}
+          <div className='flex  item-center space-x-4 '>
+            {isAuthenticated ? (
+              <>
+                <Link to="/" className='hover:text-blue-200'>Dashboard</Link>
+                <span className='text-white mx-4'>
+                  Hello, {user?.name}!
+                </span>
+                <button onClick={handleLogout} className='bg-red-500 hover:bg-red-600 text-white px-3 py-1 rounded'>Logout</button>
+              </>
+            ) : (
+              <>
+                <div className='space-x-4 flex  items-center  font-semibold'>
+                  <Link to="/login" className="hover:text-blue-200 ">
+                    Login
+                  </Link>
+                  <Link
+                    to="/register"
+                    className="bg-teal-700 hover:bg-teal-800 px-3 py-1 rounded"
+                  >
+                    Register
+                  </Link>
+                </div>
+              </>
+            )}
+          </div>
+        </div>
+      </div>
+    </nav>
   )
 }
 
